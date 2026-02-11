66162a5d8a3951da64ac5679d10edd5077501ddf
Kayla <kaylai.johnson@yahoo.com>
2026-02-11 12:13:27 -0500

    Solved leson 8 using filter and loops

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
 const removeFromArray = function() {
+    let argus = Array.from(arguments);
+    let arr = argus[0];
+    let ans = {};
+    for(let i = 1; i < argus.length; i++){
+        arr = arr.filter(num => num !== argus[i]);
+    }
+    return arr;
 };
 
 // Do not edit below this line
