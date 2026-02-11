9681688a6b0f0144bfcc9f4ff2942b54c6bb6098
Kayla <kaylai.johnson@yahoo.com>
2026-02-11 11:37:10 -0500

    completed lesson 7 with for loops

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let ans = "";
+    for(let i = 0; i < word.length; i++){
+        ans = word[i] + ans;
+    }
+    return ans;
 };
 
 // Do not edit below this line
