e9e12c75721df3f815d634fa7ac7ef81eb639244
Kayla <kaylai.johnson@yahoo.com>
2026-02-11 12:42:41 -0500

    completed lesson 9 using math logic

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if(!Number.isInteger(a) || !Number.isInteger(b) || a < 0 || b < 0) return 'ERROR';
+    let lb = a;
+    let ub = b;
+    if(a > b){
+        lb = b;
+        ub = a;
+    }
+    let ans = ((ub - lb + 1)*(ub + lb))/2;
 
+    console.log(ans);
+
+    return ans;
 };
 
 // Do not edit below this line
